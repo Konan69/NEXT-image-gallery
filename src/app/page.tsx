a8fea23ae990bb1eef7ddf6b9b3829629bb0994a
Konan <kixeyems0@gmail.com>
2024-07-16 23:52:43 +0100

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Alert } from "react-bootstrap";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Alert>
+        This application is a Next.js image gallery that allows users to search
+        for images based on specific queries. It utilizes client-side rendering
+        to fetch data without exposing sensitive information, and server-side
+        rendering for user and topic profile pages. The app also dynamically
+        generates metadata for profile pages and caches static pages at build
+        time for improved performance. Overall, the app provides a seamless and
+        efficient way for users to explore and interact with images on Unsplash.
+      </Alert>
+    </div>
+  );
 }
